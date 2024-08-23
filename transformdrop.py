25f90b5ab633acdc5a24c73a2097c58fd0ad2314
Jane Chen <janechen@cs.utexas.edu>
2024-08-23 10:48:13 -0500

    Avoid rewriting old data for transformdrop

--- transformdrop.py
+++ transformdrop.py
@@ -8,15 +8,16 @@ new_dataset_name = dataset_name + '-' + str(column_index) + '-sum'
 
 with open('./NEWDatasets/'+dataset_name+'.p', 'rb') as f:
     train_dataset = pickle.load(f)
+    new_train_dataset = torch.zeros_like(train_dataset)
 
     # Replace each value in the 6th column with the sum of the last 10 values
     for i in range(train_dataset.shape[0]):
         for j in range(train_dataset.shape[1]):
             if j >= 10:
-                train_dataset[i, j, column_index] = torch.sum(train_dataset[i, j-10:j, column_index])
+                new_train_dataset[i, j, column_index] = torch.sum(train_dataset[i, j-10:j, column_index])
             else:
-                train_dataset[i, j, column_index] = torch.sum(train_dataset[i, :j, column_index])
+                new_train_dataset[i, j, column_index] = torch.sum(train_dataset[i, :j, column_index])
                 
 # Save the selected 80k samples
 with open('./NEWDatasets/'+new_dataset_name+'.p', 'wb') as f:
-    pickle.dump(train_dataset, f, pickle.HIGHEST_PROTOCOL)
+    pickle.dump(new_train_dataset, f, pickle.HIGHEST_PROTOCOL)
